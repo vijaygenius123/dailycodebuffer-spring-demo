3bb5851471db16c419322de042c1709cf3d26914
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2022-11-08 17:16:27 +0000

    Update properties

--- src/main/java/com/vijaygenius123/dailycodebufferspringdemo/service/EmployeeV2ServiceImpl.java
+++ src/main/java/com/vijaygenius123/dailycodebufferspringdemo/service/EmployeeV2ServiceImpl.java
@@ -1,11 +1,14 @@
 package com.vijaygenius123.dailycodebufferspringdemo.service;
 
+import com.vijaygenius123.dailycodebufferspringdemo.entity.EmployeeEntity;
 import com.vijaygenius123.dailycodebufferspringdemo.model.Employee;
 import com.vijaygenius123.dailycodebufferspringdemo.repository.EmployeeRepository;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.UUID;
 
 @Service
 public class EmployeeV2ServiceImpl implements EmployeeService{
@@ -14,7 +17,14 @@ public class EmployeeV2ServiceImpl implements EmployeeService{
     private EmployeeRepository employeeRepository;
     @Override
     public Employee save(Employee employee) {
-        return null;
+        if(employee.getEmployeeId()== null){
+            employee.setEmployeeId(UUID.randomUUID().toString());
+        }
+
+        EmployeeEntity employeeEntity = new EmployeeEntity();
+        BeanUtils.copyProperties(employee, employeeEntity);
+        employeeRepository.save(employeeEntity);
+        return employee;
     }
 
     @Override
